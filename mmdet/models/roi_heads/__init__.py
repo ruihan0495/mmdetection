074ed37aea17b8596264e9e5cae9ba6796457a48
ruihan0495 <34216537+ruihan0495@users.noreply.github.com>
2021-04-15 12:04:48 +0200

    init roi head

--- mmdet/models/roi_heads/__init__.py
+++ mmdet/models/roi_heads/__init__.py
@@ -15,6 +15,7 @@ from .roi_extractors import SingleRoIExtractor
 from .shared_heads import ResLayer
 from .standard_roi_head import StandardRoIHead
 from .trident_roi_head import TridentRoIHead
+from .cascade_pointrend_head.py import CascadePointRendRoIHead
 
 __all__ = [
     'BaseRoIHead', 'CascadeRoIHead', 'DoubleHeadRoIHead', 'MaskScoringRoIHead',
@@ -23,5 +24,5 @@ __all__ = [
     'Shared4Conv1FCBBoxHead', 'DoubleConvFCBBoxHead', 'FCNMaskHead',
     'HTCMaskHead', 'FusedSemanticHead', 'GridHead', 'MaskIoUHead',
     'SingleRoIExtractor', 'PISARoIHead', 'PointRendRoIHead', 'MaskPointHead',
-    'CoarseMaskHead', 'DynamicRoIHead', 'TridentRoIHead'
+    'CoarseMaskHead', 'DynamicRoIHead', 'TridentRoIHead', 'CascadePointRendROIHead'
 ]
